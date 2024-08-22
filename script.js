f9a44a6a8b31bb1389508dd1c860a2d2d98f4f86
Shubham Singh <119438226+Shubham5925@users.noreply.github.com>
2024-08-22 18:24:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function changetheme() 
+{ 
+	var color=document.getElementById('select').value;
+	/*if (color=="dark")
+	{
+		document.body.style.backgroundColor="black";
+	}
+	else if (color=='black')
+	{
+		document.body.style.backgroundColor="black";
+	}
+	else if (color=='red')
+	{
+		document.body.style.backgroundColor="red";
+	}
+	else if (color=='lightblue')
+	{
+		document.body.style.backgroundColor="lightblue";
+	}
+	else if (color=='green')
+	{
+		document.body.style.backgroundColor="green";
+	}
+	else if (color=='pink')
+	{
+		document.body.style.backgroundColor="pink";
+	}
+	*/
+}
